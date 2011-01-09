bdd131aba121a0e8b1f8b534e114ab03b8d51ff1
Steve Yu <suguni@gmail.com>
2011-01-09 22:43:23 +0900

    ex 2.18, 2.20 추가

--- ch.2.2.rkt
+++ ch.2.2.rkt
@@ -41,3 +41,44 @@
   (if (null? items)
       items
       (append (reverse (cdr items)) (list (car items)))))
+;; 이거 너무 속도가 느린거 같은데.. n^2
+
+;; append를 사용하지 않는 버전 ???
+
+
+
+
+;; ex 2.19 -- skip
+
+
+;; ex 2.20
+
+;; dotted-tail notation
+;; (define (f x y . z) (apply + z))
+
+;; (same-parity 1 4 5 6 7 8 9) -> (1 5 7 9)
+;; (same-parity 2 4 5 6 7 8 9) -> (2 4 6 8)
+(define (same-parity x . items)
+  (define (iter i o pred?)
+    (if (null? i)
+        o
+        (iter (cdr i)
+              (if (pred? (car i))
+                  (cons (car i) o)
+                  o)
+              pred?)))
+  (iter items (list x)
+        (if (even? x) even? odd?)))
+;; 이거 순서가 이상하다.
+
+(define (same-parity-2 x . items)
+  (let ((pred? (if (even? x) even? odd?)))
+    (define (iter i)
+      (if (null? i)
+          (list)
+          (if (pred? (car i))
+              (cons (car i) (iter (cdr i)))
+              (iter (cdr i)))))
+    (cons x (iter items))))
+;; OK!
+
