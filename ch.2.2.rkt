a9aa4d592497172c65211177c1f15cc52276e886
Steve Yu <suguni@gmail.com>
2011-01-11 00:05:48 +0900

    - ex 2.17 last-pair 프로시저에서 인자가 NULL 인 경우 오류 처리한 프로시저로 변경 - ex 2.18 append 프로시저를 사용하지 않는 reverse 프로시저 추가 - ex 2.23 추가
    Signed-off-by: Steve Yu <suguni@gmail.com>

--- ch.2.2.rkt
+++ ch.2.2.rkt
@@ -1,5 +1,9 @@
 #lang racket
 
+;; common procedure
+(define (square x)
+  (* x x))
+
 (define (list-ref items n)
   (if (= n 0)
       (car items)
@@ -31,10 +35,20 @@
 (define i2 (list 'd 'e 'f))
 
 ;; ex 2.17
+;(define (last-pair items)
+;  (if (null? (cdr items))
+;      items
+;      (last-pair (cdr items))))
+;; 위 코드는 (last-pair (list)) 이면 에러
 (define (last-pair items)
-  (if (null? (cdr items))
+  (define (iter l)
+    (let ((rest (cdr l)))
+      (if (null? rest)
+          l
+          (iter rest))))
+  (if (null? items)
       items
-      (last-pair (cdr items))))
+      (iter items)))
 
 ;; ex 2.18
 (define (reverse items)
@@ -42,14 +56,16 @@
       items
       (append (reverse (cdr items)) (list (car items)))))
 ;; 이거 너무 속도가 느린거 같은데.. n^2
-
 ;; append를 사용하지 않는 버전 ???
-
-
-
+(define (reverse-2 items)
+  (define (iter ins outs)
+    (if (null? ins)
+        outs
+        (iter (cdr ins) (cons (car ins) outs))))
+  (iter items (list)))
+;; recursive 하게 짜는건 포기. append를 사용하지 않고는 힘듬.
 
 ;; ex 2.19 -- skip
-
 
 ;; ex 2.20
 
@@ -82,8 +98,6 @@
     (cons x (iter items))))
 ;; OK!
 
-(define (square x)
-  (* x x))
 
 ;; p.136
 (define (map proc items)
@@ -96,8 +110,64 @@
 (define (square-list-1 items)
   (if (null? items)
       (list)
-      (cons (* (car items) (car items))
+      (cons (* (car items) (car items)) ;; (square (car items)) 로 해도 됨.
             (square-list-1 (cdr items)))))
 
 (define (square-list-2 items)
   (map (lambda (x) (* x x)) items))
+
+;; ex 2.22
+(define (square-list-iter-1 items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons (square (car things)) answer))))
+  (iter items (list)))
+;; 위 코드는 순서가 반대로 나온다. 왜?
+;; => (cons (square (car things)) answer) 때문. 이렇게 하면 뒤에서 부터 채워진다.
+;; (square-list-iter-1 (list 1 2 3 4)) 이면, answer가 만들어지는 방식은
+;; (cons 1 ())
+;; (cons 4 (1))
+;; (cons 9 (4 1))
+;; (cons 16 (9 4 1))
+;; (16 9 4 1)
+;; 이 된다.
+
+(define (square-list-iter-2 items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons answer (square (car things))))))
+  (iter items (list)))
+;; 이렇게 하면 list 요소가 list가 된다.
+;; (square-list-iter-2 (list 1 2 3 4)) => ((((() . 1) . 4) . 9) . 16)
+;; cons의 첫번째 인자가 list 이기 때문.
+
+;; ex 2.23
+;; for-each 프로시저 짜기.
+(define (for-each proc items)
+  (if (null? items)
+      (list)
+      (let ((dummy (proc (car items))))
+        (for-each proc (cdr items)))))
+;; proc 프로시저를 실행할 방법이 필요한데, 위에서는 let을 이용함.
+;; map에서 했던거 처럼 cons를 사용하는데 대신 의미 없음.
+(define (for-each-1 proc items)
+  (if (null? items)
+      (list)
+      (cons (proc (car items))
+            (for-each-1 proc (cdr items)))))
+;; http://community.schemewiki.org/?sicp-ex-2.23에서는 if 대신 cond를 사용함.
+;; if는 true와 false에 하나의 form만 올 수 있는데 반해 cond는 여러개의 form이 올 수 있음.
+(define (for-each-2 proc items)
+  (cond ((not (null? items))
+         (proc (car items))
+         (for-each-2 proc (cdr items)))))
+;; Racket에서는 begin special form을 사용할 수 있다. side-effect가 필요한 경우 사용.
+(define (for-each-4 proc items)
+  (if (null? items)
+      (list)
+      (begin (proc (car items))
+             (for-each-4 proc (cdr items)))))
