5a2df78c46d0c9cd15df616793ce1cf7bf2a786f
Hyun-chul Jeon <chorr@Chorr-MacBook-Air.local>
2011-01-13 02:03:38 +0900

    ex2.19 추가 후 커밋

--- ch2.2.rkt
+++ ch2.2.rkt
@@ -65,7 +65,30 @@
   (iter items (list)))
 ;; recursive 하게 짜는건 포기. append를 사용하지 않고는 힘듬.
 
-;; ex 2.19 -- skip
+;; ex 2.19
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else (+ (cc amount
+                     (except-first-denomination coin-values))
+                 (cc (- amount
+                        (first-denomination coin-values))
+                     coin-values)))))
+
+(define (no-more? coin-values)
+  (null? coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define us-coins (list 50 25 10 5 1))
+(define us-coins-2 (list 1 5 10 25 50))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+(define uk-coins-2 (list 0.5 1 2 5 10 20 50 100))
+;; 역순으로도 정상적인 결과를 보여준다. (어차피 모든 경우를 탐색하기 때문)
 
 ;; ex 2.20
 
