25f74ee681ca89a03f193ea12b42347479b895a1
Steve Yu <suguni@gmail.com>
2011-02-02 00:02:39 +0900

    그림틀 추가
    Signed-off-by: Steve Yu <suguni@gmail.com>

--- ch2.2.4.rkt
+++ ch2.2.4.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+;; references http://planet.racket-lang.org/package-source/soegaard/sicp.plt/2/1/planet-docs/sicp-manual/index.html
 (require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
 
 ;; original source - http://d.hatena.ne.jp/kazu-yamamoto/20100524/1274666251
@@ -102,3 +103,73 @@
 ;(paint (up-split-2 wave 4))
 (define right-split-2 (split below beside))
 ;(paint (right-split-2 wave 4))
+
+
+;; 그림틀
+
+;; ex 2.46
+(define (make-vector x y)(cons x y))
+(define (xcor-vect v) (car v))
+(define (ycor-vect v) (cdr v))
+
+(define (add-vect v1 v2)
+  (make-vector (+ (xcor-vect v1) (xcor-vect v2))
+               (+ (ycor-vect v1) (ycor-vect v2))))
+(define (sub-vect v1 v2)
+  (make-vector (- (xcor-vect v1) (xcor-vect v2))
+               (- (ycor-vect v1) (ycor-vect v2))))
+(define (scale-vect s v)
+  (make-vector (* (xcor-vect v) s)
+               (* (ycor-vect v) s)))
+
+;; test
+;(= (xcor-vect (make-vector 1 2)) 1)
+;(= (ycor-vect (make-vector 1 2)) 2)
+;(equal? (add-vect (make-vector 1 5) (make-vector 3 2)) (make-vector 4 7))
+;(equal? (sub-vect (make-vector 1 5) (make-vector 3 2)) (make-vector -2 3))
+;(equal? (scale-vect (make-vector 1 5) 3) (make-vector 3 15))
+
+;; ex 2.47
+(define (make-frame-2-1 origin edge1 edge2) (list origin edge1 edge2))
+(define (origin-frame-2-1 frame) (car frame))
+(define (edge1-frame-2-1 frame) (car (cdr frame)))
+(define (edge2-frame-2-1 frame) (car (cdr (cdr frame))))
+;; test
+(let ((o (make-vector 3 3))
+      (e1 (make-vector 10 2))
+      (e2 (make-vector 2 10)))
+  (let ((f (make-frame-2-1 o e1 e2)))
+    (format "~a ~a ~a"
+            (equal? (origin-frame-2-1 f) o)
+            (equal? (edge1-frame-2-1 f) e1)
+            (equal? (edge2-frame-2-1 f) e2))))
+
+(define (make-frame-2-2 origin edge1 edge2) (cons origin (cons edge1 edge2)))
+(define (origin-frame-2-2 frame) (car frame))
+(define (edge1-frame-2-2 frame) (car (cdr frame)))
+(define (edge2-frame-2-2 frame) (cdr (cdr frame)))
+;; test
+(let ((o (make-vector 3 3))
+      (e1 (make-vector 10 2))
+      (e2 (make-vector 2 10)))
+  (let ((f (make-frame-2-2 o e1 e2)))
+    (format "~a ~a ~a"
+            (equal? (origin-frame-2-2 f) o)
+            (equal? (edge1-frame-2-2 f) e1)
+            (equal? (edge2-frame-2-2 f) e2))))
+
+(define make-frame-2 make-frame-2-1)
+(define origin-frame-2 origin-frame-2-1)
+(define edge1-frame-2  edge1-frame-2-1)
+(define edge2-frame-2  edge2-frame-2-1)
+
+(define (frame-coord-map-2 frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame-2 frame)
+     (add-vect (scale-vect (xcor-vect v)
+                           (edge1-frame-2 frame))
+               (scale-vect (ycor-vect v)
+                           (edge2-frame-2 frame))))))
+
+;; 페인터
