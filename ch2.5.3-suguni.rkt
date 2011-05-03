fa59e9dc6fa2887484e26201f6dee5996c845b9c
Jeon Hyun-chul <julyfool@gmail.com>
2011-05-03 21:28:42 +0900

    스터디중 수정분량

--- ch2.5.3-suguni.rkt
+++ ch2.5.3-suguni.rkt
@@ -273,7 +273,7 @@
   ;; 다른 코드(adjoin-term을 이용하는 프로시저들)는 그대로 두고 adjoin-term 프로시저만 변경
   ;; 아래 테스트 코드에서는 sparse term-list로 테스트하고 있어 수정함.
   (define (adjoin-term term term-list)
-    (define (insert-n v n l)
+    (define (insert-n v n l) ;; l=(1 2 3 4) v=2, n=3 => (2 2 2 1 2 3 4)
       (if (= n 0)
           l
           (insert-n v (- n 1) (cons v l))))
@@ -293,8 +293,13 @@
   
   (define (the-empty-termlist) '())
   (define (empty-termlist? term-list) (null? term-list))
-  (define (first-term term-list) (make-term (- (length term-list) 1) (car term-list))) ;; !!!
-  (define (rest-terms term-list) (cdr term-list))
+  (define (first-term term-list)
+    (make-term (- (length term-list) 1) (car term-list))) ;; !!!
+  (define (rest-terms term-list)
+    (cdr term-list))
+
+  
+  ;; (put 'terms 'polynomial (lambda(x) (term-list x)))
   
   (define (make-term order coeff) (list order coeff))
   (define (order term) (car term))
@@ -371,7 +376,8 @@
   
   ;; ex 2.88
   (define (sub-terms l1 l2)
-    (let ((minus-l2 (mul-terms '((0 -1)) l2)))
+    (let ((minus-l2 (mul-terms (adjoin-term (make-term 0 -1)
+                                            (the-empty-termlist)) l2))) ;; ((0 -1))
       (add-terms l1 minus-l2)))
   (define (sub-poly p1 p2)
     (if (same-variable? (variable p1) (variable p2))
@@ -397,8 +403,8 @@
 ;; polynomial package test code
 
 ;; sparse term list
-;(define p1 (make-polynomial 'x '((2 1) (1 2) (0 1))))  ;; x^2 + 2x + 1
-;(define p2 (make-polynomial 'x '((2 1) (1 -2) (0 1)))) ;; x^2 - 2x + 1
+;; (define p1 (make-polynomial 'x '((2 1) (1 2) (0 1))))  ;; x^2 + 2x + 1
+;; (define p2 (make-polynomial 'x '((2 1) (1 -2) (0 1)))) ;; x^2 - 2x + 1
 
 ;; dense term list 인 경우
 (define p1 (make-polynomial 'x '(1 2 1)))  ;; x^2 + 2x + 1
@@ -407,8 +413,8 @@
 
 ;; ex 2.87 test code
 (define zero-poly1 (make-polynomial 'x '()))
-;; (define zero-poly2 (make-polynomial 'x '((2 0) (0 0))))
-(define zero-poly2 (make-polynomial 'x '(0 0 0)))
+;(define zero-poly2 (make-polynomial 'x '((2 0) (0 0))))
+(define zero-poly2 (make-polynomial 'x '(0 0 0))) ;; 0x^2 + 0x + 0
 (=zero? zero-poly1)
 (=zero? zero-poly2)
 (=zero? p1)
@@ -456,3 +462,6 @@
 ;  
 ;  
 ;  'done)
+
+;;(define py (make-polynomial 'y '((2 1) (0 1))))
+(define py (make-polynomial 'y '(1 0 1)))
