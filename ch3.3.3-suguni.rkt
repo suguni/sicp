298b7708876b86ca42a3f757b1c348a1cc37a794
Steve Yu <suguni@gmail.com>
2011-06-07 21:07:37 +0900

    연습문제 3.24, 3.25 추가

--- ch3.3.3-suguni.rkt
+++ ch3.3.3-suguni.rkt
@@ -3,7 +3,7 @@
 (define (c-assoc key records)
   (cond ((null? records) #f)
         ((equal? (car (car records)) key) (car records))
-        (else (assoc key (cdr records)))))
+        (else (c-assoc key (cdr records)))))
 
 (define (make-table)
   (list '*table* ))
@@ -15,7 +15,7 @@
 ;        #f)))
 ;
 ;(define (insert! key value table)
-;  (let ((record (assoc key (cdr table))))
+;  (let ((record (c-assoc key (cdr table))))
 ;    (if record
 ;        (set-cdr! record value)
 ;        (set-cdr! table
@@ -43,7 +43,7 @@
 (define (insert! key-1 key-2 value table)
   (let ((sub-table (c-assoc key-1 (cdr table))))
     (if sub-table
-        (let ((record (assoc key-2 (cdr sub-table))))
+        (let ((record (c-assoc key-2 (cdr sub-table))))
           (if record
               (set-cdr! record value)
               (set-cdr! sub-table
@@ -82,7 +82,7 @@
     (define (insert! key-1 key-2 value)
       (let ((sub-table (c-assoc key-1 (cdr local-table))))
         (if sub-table
-            (let ((record (assoc key-2 (cdr sub-table))))
+            (let ((record (c-assoc key-2 (cdr sub-table))))
               (if record
                   (set-cdr! record value)
                   (set-cdr! sub-table
@@ -99,6 +99,129 @@
   
     dispatch))
 
-(define operation-table (make-table))
-(define get (operation-table 'lookup-proc))
-(define put (operation-table 'insert-proc!))
+;(define operation-table (make-table-mp))
+;(define get (operation-table 'lookup-proc))
+;(define put (operation-table 'insert-proc!))
+
+;; ex 3.24
+;; 그냥 assoc 을 내부에 숨기고, assoc이 equal? 대신 인자로 받은 same-key?를 사용하도록 수정.
+;; 이렇게 하는건 너무 간단하자나!!! 다른 방법이 있나???
+(define (make-table-sk same-key?)
+
+  (define (mt-assoc key records)
+    (cond ((null? records) #f)
+          ((same-key? (car (car records)) key) (car records))
+          (else (mt-assoc key (cdr records)))))
+
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((sub-table (mt-assoc key-1 (cdr local-table))))
+        (if sub-table
+            (let ((record (mt-assoc key-2 (cdr sub-table))))
+              (if record
+                  (cdr record)
+                  #f))
+            #f)))
+
+    (define (insert! key-1 key-2 value)
+      (let ((sub-table (mt-assoc key-1 (cdr local-table))))
+        (if sub-table
+            (let ((record (mt-assoc key-2 (cdr sub-table))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! sub-table
+                            (cons (cons key-2 value) (cdr sub-table)))))
+            (set-cdr! local-table
+                      (cons (list key-1 (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)
+    
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation -- TABLE" m))))
+  
+    dispatch))
+
+;(define operation-table (make-table-sk equal?))
+;(define get (operation-table 'lookup-proc))
+;(define put (operation-table 'insert-proc!))
+;
+;(put 'math 'plus +)
+;(put 'math 'minus -)
+;((get 'math 'plus) 1 2 3)
+
+
+;; 3.25 아래처럼 여러개의 열쇠가 가능하도록 하고 싶다.
+;; (define my-table (make-table))
+;; (insert! '(k1 k2 k3 k4) "v1" my-table)
+;; (lookup '(k1 k2 k3 k4) my-table)
+
+(define (make-table-mp)
+
+  (let ((local-table (list '*table*)))
+    
+    (define (lookup keys)
+      (define (iter ks table)
+        (if (or (null? ks) (null? table))
+            #f
+            (let ((record (c-assoc (car ks) (cdr table))))
+              (if record
+                  (if (pair? (cdr record))
+                      (iter (cdr ks) record)
+                      (cdr record))
+                  #f))))
+      (iter keys local-table))
+
+    (define (insert! keys value)
+
+      (define (make-sub-table ks)
+        (if (null? (cdr ks))
+            (cons (car ks) value)
+            (list (car ks) (make-sub-table (cdr ks)))))
+
+      (define (iter! ks table)
+        (if (null? ks)
+            (error "NULL error" ks)
+            (if (pair? (cdr table))
+                (let ((record (c-assoc (car ks) (cdr table))))
+                  (if record
+                      (if (null? (cdr ks))
+                          (set-cdr! record value)
+                          (iter! (cdr ks) record))
+                      (set-cdr! table
+                                (cons (make-sub-table ks) (cdr table)))))
+                (set-cdr! table
+                          (cons (make-sub-table ks) '())))))
+      (iter! keys local-table)
+      'ok)
+    
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            ((eq? m 'table) local-table)
+            (else (error "Unknown operation -- TABLE" m))))
+  
+    dispatch))
+
+(define tab1 (make-table-mp))
+(define get (tab1 'lookup-proc))
+(define put (tab1 'insert-proc!))
+
+(put '(a a a) 1)
+(put '(a a b) 2)
+(put '(a a c) 3)
+(put '(a b a) 4)
+(put '(a b b) 5)
+(put '(a b c) 6)
+(put '(a c a) 7)
+(put '(a c b) 8)
+(put '(a c c) 9)
+
+(get '(a a c)) ;; >> 3
+
+;; 아래처럼 입력하고
+(put '(a a a b b b c c c) 12345)
+;; 아래는?
+(get '(a a a))
+;; 이상하다.
